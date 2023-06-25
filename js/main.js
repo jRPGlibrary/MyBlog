3bf105e47bb9d9832b4bf0c98792dfd180535152
jRPGlibrary <contentcreator.jrpglibrary@gmail.com>
2023-06-25 11:17:44 +0200

    Update Footer petit ecran présentation images mobile

--- js/main.js
+++ js/main.js
@@ -1,32 +1,28 @@
-window.onload = function () {
-  // Dates de publication des articles
-  var post1Date = new Date("2023-06-15"); // Remplacez avec votre propre date
-  var post2Date = new Date("2023-06-15"); // Remplacez avec votre propre date
-  var post3Date = new Date("2023-06-15"); // Remplacez avec votre propre date
-  var post4Date = new Date("2023-06-15"); // Remplacez avec votre propre date
+window.onload = function() {
+  // Obtention de la date actuelle
+  var currentDate = new Date();
+
+  // Dates de publication des articles (dans l'ordre chronologique)
+  var postDates = [
+    new Date("2023-06-24"),
+    new Date("2023-06-22"),
+    new Date("2023-06-22"),
+    new Date("2023-06-10")
+  ];
 
   // Calcul du nombre de jours écoulés depuis la publication de chaque article
-  var currentDate = new Date();
-  var timeDiff1 = Math.abs(currentDate.getTime() - post1Date.getTime());
-  var timeDiff2 = Math.abs(currentDate.getTime() - post2Date.getTime());
-  var timeDiff3 = Math.abs(currentDate.getTime() - post3Date.getTime());
-  var timeDiff4 = Math.abs(currentDate.getTime() - post4Date.getTime());
-
-  var daysSincePost1 = Math.ceil(timeDiff1 / (1000 * 3600 * 24));
-  var daysSincePost2 = Math.ceil(timeDiff2 / (1000 * 3600 * 24));
-  var daysSincePost3 = Math.ceil(timeDiff3 / (1000 * 3600 * 24));
-  var daysSincePost4 = Math.ceil(timeDiff4 / (1000 * 3600 * 24));
+  var daysSincePosts = postDates.map(function(postDate) {
+    var timeDiff = Math.abs(currentDate.getTime() - postDate.getTime());
+    return Math.ceil(timeDiff / (1000 * 3600 * 24));
+  });
 
   // Mettre à jour les éléments HTML avec les nombres de jours
-  document.getElementById("daysSincePost1").innerHTML =
-    daysSincePost1 + " jours";
-  document.getElementById("daysSincePost2").innerHTML =
-    daysSincePost2 + " jours";
-  document.getElementById("daysSincePost3").innerHTML =
-    daysSincePost3 + " jours";
-  document.getElementById("daysSincePost4").innerHTML =
-    daysSincePost4 + " jours";
-
+  for (var i = 0; i < daysSincePosts.length; i++) {
+    var postId = "daysSincePost" + (i + 1);
+    document.getElementById(postId).innerHTML =
+      daysSincePosts[i] + " jours";
+  }
+  
   // Mettre à jour les noms des auteurs
   document.querySelector("#NewsHeader_Border1 .authorName").innerHTML =
     "jRPG_Library";
