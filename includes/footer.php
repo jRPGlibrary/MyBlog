5022aee6dfbcde333d3f20e277479df46ee16f78
jRPGlibrary <contentcreator.jrpglibrary@gmail.com>
2023-06-27 09:06:21 +0200

    update partenariat, footer.css and navbar as header.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,27 @@
     <footer>
         <div id="footer_content">
-            <div id="footer_left">
+            <div class="footer_column" id="footer_left">
                 <h3>Liens utiles</h3>
                 <ul>
-                    <li><a href="#accueil">Accueil</a></li>
-                    <li><a href="#NeWs">News</a></li>
-                    <li><a href="#Previews">Previews</a></li>
-                    <li><a href="#Reviews">Reviews</a></li>
-                    <li><a href="#Partenariats">Partenariats</a></li>
+                    <li><a href="index.html">Accueil</a></li>
+                    <li><a href="actus.htmll">Actus</a></li>
+                    <li><a href="#Avant-Premières">Avant-Premières</a></li>
+                    <li><a href="#Tests">Tests</a></li>
+                    <li><a href="partenariats.html">Partenariats</a></li>
                     <li><a href="#Contacts">Contacts</a></li>
                 </ul>
             </div>
-            <div id="footer_right">
+            <div class="footer_column" id="footer_right">
                 <h3>Contacts</h3>
-                <p>Email : loremipsum@lorem.ipsum</p>
+                <p>Email: loremipsum@lorem.ipsum</p>
+            </div>
+            <div class="footer_column" id="footer_twitch">
+                <div id="Twitch_Container">
+                    <div id="Twitch_Embed"></div>
+                </div>
             </div>
         </div>
         <div id="footer_bottom">
-            <p>&copy; 2023 LeBlogueurFou. Tous droits réservés.</p>
+            <p>&copy; 2023 jRPGLibrary. Tous droits réservés.</p>
         </div>
     </footer>
