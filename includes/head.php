61770e69c4394d53ee0b9198ec828463c889c66b
jRPGlibrary <contentcreator.jrpglibrary@gmail.com>
2023-07-08 17:32:54 +0200

    refresh CSS, suppression de accueil

--- includes/head.php
+++ includes/head.php
@@ -5,11 +5,14 @@
     <title> jRPGlibrary - Accueil </title>
 
     <link rel="stylesheet" href="./css/reset.css">
+
     <link rel="stylesheet" href="./css/header.css">
     <link rel="stylesheet" href="./css/social_menu.css">
-    <link rel="stylesheet" href="./css/news_header.css">
+
     <link rel="stylesheet" href="./css/partenariats.css">
+
     <link rel="stylesheet" href="./css/twitch.css">
+
     <link rel="stylesheet" href="./css/footer.css">
 
     <script src="https://embed.twitch.tv/embed/v1.js"></script>
