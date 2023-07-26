b58894e746d03825985b3ff3754739a375dc02b2
Patfine <140624713+Patfine@users.noreply.github.com>
2023-07-26 21:51:48 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,26 @@
+<footer id="Footer_Container">
+    <div class="column">
+        <!-- Ajoutez ici les liens vers vos autres pages -->
+        <ul>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+        </ul>
+    </div>
+    <div class="column">
+        <!-- Ajoutez ici le lecteur Twitch -->
+        <div id="Twitch_Embed"></div>
+    </div>
+    <div class="column">
+        <!-- Ajoutez ici vos réseaux sociaux et votre adresse e-mail -->
+        <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Instagram</a></li>
+        </ul>
+        <p>votre@mail.com</p>
+    </div>
+    <div>
+        <p>&copy; 2023 jRPGLibrary. Tous droits réservés.</p>
+    </div>
+</footer>
---
+++ js/twitch.js
@@ -0,0 +1,8 @@
+new Twitch.Embed("Twitch_Embed", {
+  width: "100%",
+  height: "200px", // Ajustez la hauteur du lecteur Twitch en fonction de vos besoins
+  channel: "jRPG_Library",
+  autoplay: true,
+  theme: "dark",
+  layout: "video",
+});
