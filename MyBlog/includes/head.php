f1aa3567a22efca6c8355488a6a1b7d321af66f5
Patfine <140624713+Patfine@users.noreply.github.com>
2023-07-26 19:40:35 +0200

    Add files via upload

---
+++ MyBlog/includes/head.php
@@ -0,0 +1,22 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title> jRPGlibrary - Accueil </title>
+
+    <link rel="stylesheet" href="./css/reset.css">
+
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/social_menu.css">
+
+    <link rel="stylesheet" href="./css/partenariats.css">
+
+    <link rel="stylesheet" href="./css/twitch.css">
+
+    <link rel="stylesheet" href="./css/footer.css">
+
+    <script src="https://embed.twitch.tv/embed/v1.js"></script>
+
+    <link rel="shortcut icon" href="./images/Logo.png" type="image/x-icon">
+
+</head>
