01b78081a4666f7aeb6baccdfd04b28939be6a28
jRPGlibrary <contentcreator.jrpglibrary@gmail.com>
2023-09-23 17:39:20 +0200

    php Eiyuden
    variable du contenu meta eiyuden

---
+++ includes/meta-Eiyuden-Chronicle.php
@@ -0,0 +1,26 @@
+<?php
+// Variables pour les balises méta
+$siteTitle = "jRPGLibrary";
+$siteDescription = "Le successeur de Suikoden annonce sa date de sortie lors du Nintendo Direct. Faisons le point.";
+$siteImageURL = "https://nintendoeverything.com/wp-content/uploads/Eiyuden-Chronicle-Hundred-Heroes-delayed.jpg";
+
+// Variables pour les balises Open Graph
+$ogTitle = "Eiyuden Chronicle: Hundred Heroes dévoile sa date de sortie";
+$ogDescription = "Le successeur de Suikoden annonce sa date de sortie lors du Nintendo Direct. Faisons le point.";
+$ogImageURL = "https://nintendoeverything.com/wp-content/uploads/Eiyuden-Chronicle-Hundred-Heroes-delayed.jpg";
+$ogURL = "https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html";
+$ogSiteName = "jRPGLibrary";
+$ogType = "article";
+$ogLocale = "fr_FR";
+$ogAuthor = "Patfine";
+$ogPublishedTime = "2023-09-17T15:00:00+00:00";
+
+// Variables pour les balises Twitter Card
+$twitterTitle = "Eiyuden Chronicle: Hundred Heroes dévoile sa date de sortie";
+$twitterDescription = "Le successeur de Suikoden annonce sa date de sortie lors du Nintendo Direct. Faisons le point.";
+$twitterImageURL = "https://nintendoeverything.com/wp-content/uploads/Eiyuden-Chronicle-Hundred-Heroes-delayed.jpg";
+$twitterCard = "summary_large_image";
+$twitterDomain = "jRPGLibrary.com";
+$twitterCreator = "Patfine";
+$twitterCreatedAt = "2023-09-17T15:00:00+00:00";
+?>
