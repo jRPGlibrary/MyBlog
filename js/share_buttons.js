346a65c27e99f6a34a4836c30128e955abb7d1e8
jRPGlibrary <contentcreator.jrpglibrary@gmail.com>
2023-10-24 00:19:19 +0200

    update

--- js/share_buttons.js
+++ js/share_buttons.js
@@ -1,50 +1,53 @@
-const url = document.location.href;
+function shareFacebook() {
+  // Code pour partager sur Facebook
+  // Remplacez "URL_de_votre_page"
+  window.open(
+    "http://www.facebook.com/sharer/sharer.php?u=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html",
+    "_blank"
+  );
+}
 
-function shareFacebook(url) {
-    // Code pour partager sur Facebook
-    // Remplacez "URL_de_votre_page" 
-    window.open(
-      "https://www.facebook.com/sharer/sharer.php?u="+url,
-      "_blank"
-    );
-  }
-  
-  function shareMessenger(url) {
-    // Code pour partager sur Messenger
-    window.open(
-      "https://www.facebook.com/dialog/send?link="+url+"&app_id=123456789",
-      "_blank"
-    );
-  }
-  
-  function shareTwitter(url) {
-    // Code pour partager sur Twitter
-    window.open(
-      "https://twitter.com/intent/tweet?url="+url,
-      "_blank"
-    );
-  }
-  
-  function shareLinkedIn(url) {
-    // Code pour partager sur LinkedIn
-    window.open(
-      "https://www.linkedin.com/shareArticle?url="+url,
-      "_blank"
-    );
-  }
-  
-  function shareWhatsApp(url) {
-    // Code pour partager sur WhatsApp
-    window.open(
-      "https://api.whatsapp.com/send?text="+url,
-      "_blank"
-    );
-  }
-  
-  function shareReddit(url) {
-    // Code pour partager sur Reddit
-    window.open(
-      "https://www.reddit.com/submit?url="+url,
-      "_blank"
-    );
-  }
+function shareMessenger() {
+  // Code pour partager sur Messenger
+
+  window.open(
+    "http://www.facebook.com/dialog/send?link=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html&app_id=123456789",
+    "_blank"
+  );
+}
+
+function shareTwitter() {
+  // Code pour partager sur Twitter
+
+  window.open(
+    "http://twitter.com/intent/tweet?url=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html",
+    "_blank"
+  );
+}
+
+function shareLinkedIn() {
+  // Code pour partager sur LinkedIn
+
+  window.open(
+    "http://www.linkedin.com/shareArticle?url=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html",
+    "_blank"
+  );
+}
+
+function shareWhatsApp() {
+  // Code pour partager sur WhatsApp
+
+  window.open(
+    "http://api.whatsapp.com/send?text=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html",
+    "_blank"
+  );
+}
+
+function shareReddit() {
+  // Code pour partager sur Reddit
+
+  window.open(
+    "http://www.reddit.com/submit?url=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html",
+    "_blank"
+  );
+}
