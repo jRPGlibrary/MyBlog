800825279fee0b1951b4f54be7fb1df7fcbf1283
jRPGlibrary <contentcreator.jrpglibrary@gmail.com>
2023-10-24 00:44:43 +0200

    update JS

--- js/share_buttons.js
+++ js/share_buttons.js
@@ -1,53 +1,43 @@
+const url =
+  "https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html";
+
+// Fonction pour partager sur Facebook
 function shareFacebook() {
-  // Code pour partager sur Facebook
-  // Remplacez "URL_de_votre_page"
   window.open(
-    "http://www.facebook.com/sharer/sharer.php?u=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html",
+    "http://www.facebook.com/sharer/sharer.php?u=" +
+      url +
+      "&title=Eiyuden Chronicle: Rising sort le 23 mai 2023&description=Le JRPG Eiyuden Chronicle: Rising sortira le 23 mai 2023 sur PC, PlayStation 4, PlayStation 5, Xbox One et Xbox Series X/S.",
     "_blank"
   );
 }
 
+// Fonction pour partager sur Messenger
 function shareMessenger() {
-  // Code pour partager sur Messenger
-
   window.open(
-    "http://www.facebook.com/dialog/send?link=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html&app_id=123456789",
+    "http://www.facebook.com/dialog/send?link=" + url + "&app_id=123456789",
     "_blank"
   );
 }
 
+// Fonction pour partager sur Twitter
 function shareTwitter() {
-  // Code pour partager sur Twitter
-
-  window.open(
-    "http://twitter.com/intent/tweet?url=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html",
-    "_blank"
-  );
+  window.open("http://twitter.com/intent/tweet?url=" + url, "_blank");
 }
 
+// Fonction pour partager sur LinkedIn
 function shareLinkedIn() {
-  // Code pour partager sur LinkedIn
-
   window.open(
-    "http://www.linkedin.com/shareArticle?url=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html",
+    "http://www.linkedin.com/shareArticle?mini=true&url=" + url,
     "_blank"
   );
 }
 
+// Fonction pour partager sur WhatsApp
 function shareWhatsApp() {
-  // Code pour partager sur WhatsApp
-
-  window.open(
-    "http://api.whatsapp.com/send?text=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html",
-    "_blank"
-  );
+  window.open("http://api.whatsapp.com/send?text=" + url, "_blank");
 }
 
+// Fonction pour partager sur Reddit
 function shareReddit() {
-  // Code pour partager sur Reddit
-
-  window.open(
-    "http://www.reddit.com/submit?url=https://jrpglibrary.github.io/MyBlog/pages/2023/Actus/Eiyuden-Chronicle-Date-Sortie.html",
-    "_blank"
-  );
+  window.open("http://www.reddit.com/submit?url=" + url, "_blank");
 }
